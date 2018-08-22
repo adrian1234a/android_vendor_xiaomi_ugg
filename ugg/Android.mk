d0ab4cef6e4c3f8ca757cacb766034ca428b078c
adrian1234a <adri16072002@gmail.com>
2018-08-22 14:15:21 +0200

    Fix name

--- ugg/Android.mk
+++ ugg/Android.mk
@@ -312,7 +312,7 @@ ifeq ($(LOCAL_PATH)/radio, $(wildcard $(LOCAL_PATH)/radio))
 RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
 $(foreach f, $(notdir $(RADIO_FILES)), \
     $(call add-radio-file,radio/$(f)))
-$(call add-radio-file,../../../device/xiaomi/ugglite/radio/filesmap)
+$(call add-radio-file,../../../device/xiaomi/ugg/radio/filesmap)
 
 endif
 
